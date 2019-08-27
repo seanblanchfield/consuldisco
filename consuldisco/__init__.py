0e72359608e25bf83c42d4c7d42c2eccf72c186d
Sean Blanchfield <sblanchfield@gmail.com>
2019-08-27 17:56:31 +0100

    Added option to make discovery wait until services are available, rather than returning an exception.

--- consuldisco/__init__.py
+++ consuldisco/__init__.py
@@ -1,8 +1,12 @@
 name = "consuldisco"
 
 import os
+import time
 import socket
 import requests
+import logging
+
+logger = logging.getLogger(__name__)
 
 class DiscoveryError(Exception):
     """Base class for exceptions in this module."""
@@ -18,9 +22,21 @@ def get_services(consul_host, consul_port):
    return list(json.keys())
 
 
-def discover_endpoints(service_name, consul_host=None, consul_port=None):
-   ''' Looks up a named service in Consul and returns an array of tuples describing
-       that service's endpoints in the form (hostname, IP, port).
+def discover_endpoints(service_name, consul_host=None, consul_port=None, wait=False, delay=0.1):
+   '''Looks up 'service_name' in Consul and returns an array of tuples describing
+      that service's endpoints in the form (hostname, IP, port).
+
+      Keyword arguments:
+      consul_host -- the IP or host at which consul can be found. If not specified, this should be
+                     specified in the environment variable 'CONSUL_HOST'. If not specified in either
+                     location, a DiscoveryError will be raised.
+      consul_port -- the port at which consul can be found. If not specified, this should be
+                     specified in the environment variable 'CONSUL_PORT'. If not specified in either
+                     location, a DiscoveryError will be raised.
+      wait --        Specifies that if 'service_name' is not found in consul, to block until it is 
+                     (default=False).
+      delay --       The number of seconds to wait between rechecking consul for 'service_name' if it
+                     was not found the first time, and 'wait' is True.
    ''' 
    if consul_host is None and 'CONSUL_HOST' in os.environ:
       consul_host = os.environ['CONSUL_HOST']
@@ -31,8 +47,15 @@ def discover_endpoints(service_name, consul_host=None, consul_port=None):
    else:
       raise DiscoveryError("Environment variable 'CONSUL_PORT' must be provided when 'consul_port' parameter is not provided" % service_name)
    
-   if service_name not in get_services(consul_host, consul_port):
-      raise DiscoveryError("Service '%s' not known" % service_name)
+   service_ready = False
+   while not service_ready:
+      service_ready = service_name in get_services(consul_host, consul_port)
+      if not service_ready:
+         if wait:
+            logger.info("Service {service_name} is not yet ready. Sleeping for {delay}s".format(service_name=service_name, delay=delay))
+            time.sleep(delay)
+         else:
+            raise DiscoveryError("Service '%s' not available yet" % service_name)
 
    url = "http://%s:%d/v1/health/service/%s" % (consul_host, int(consul_port), service_name)
    response = requests.get(url)
@@ -47,7 +70,19 @@ def discover_endpoints(service_name, consul_host=None, consul_port=None):
    return endpoints
 
 
-def discover_endpoint(service_name, consul_host=None, consul_port=None):
+def discover_endpoint(service_name, consul_host=None, consul_port=None, wait=False, delay=0.1):
    ''' Returns the first endpoint matching the provided service name.
+
+   Keyword arguments:
+      consul_host -- the IP or host at which consul can be found. If not specified, this should be
+                     specified in the environment variable 'CONSUL_HOST'. If not specified in either
+                     location, a DiscoveryError will be raised.
+      consul_port -- the port at which consul can be found. If not specified, this should be
+                     specified in the environment variable 'CONSUL_PORT'. If not specified in either
+                     location, a DiscoveryError will be raised.
+      wait --        Specifies that if 'service_name' is not found in consul, to block until it is 
+                     (default=False).
+      delay --       The number of seconds to wait between rechecking consul for 'service_name' if it
+                     was not found the first time, and 'wait' is True.
    '''
    return discover_endpoints(service_name, consul_host, consul_port)[0]
