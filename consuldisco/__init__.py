4243d564448b8183a45dee413311b9f820d66920
Sean Blanchfield <sblanchfield@gmail.com>
2019-08-27 19:16:37 +0100

    Split out a function to explicitly wait for a named service to be advertised.

--- consuldisco/__init__.py
+++ consuldisco/__init__.py
@@ -21,12 +21,24 @@ def get_services(consul_host, consul_port):
    json = response.json()
    return list(json.keys())
 
+def _get_consul(consul_host, consul_port):
+   if consul_host is None and 'CONSUL_HOST' in os.environ:
+      consul_host = os.environ['CONSUL_HOST']
+   else:
+      raise DiscoveryError("Environment variable 'CONSUL_HOST' must be provided when 'consul_host' parameter is not provided" % service_name)
+   if consul_port is None and 'CONSUL_PORT' in os.environ:
+      consul_port = os.environ['CONSUL_PORT']
+   else:
+      raise DiscoveryError("Environment variable 'CONSUL_PORT' must be provided when 'consul_port' parameter is not provided" % service_name)
+   return consul_host, consul_port
 
-def discover_endpoints(service_name, consul_host=None, consul_port=None, wait=False, delay=0.1):
-   '''Looks up 'service_name' in Consul and returns an array of tuples describing
-      that service's endpoints in the form (hostname, IP, port).
+def service_ready(service_name, consul_host=None, consul_port=None, wait=False, delay=0.1):
+   '''Checks if service identified by 'service_name' is being advertised by Consul. If 'wait'
+      is True, this function will block until it is ready, polling consul at an interval 
+      given in 'delay'. If 'wait' is False (default), it will raise a DiscoveryError exception.
 
       Keyword arguments:
+      service_name --the name of the service as advertised in Consul.
       consul_host -- the IP or host at which consul can be found. If not specified, this should be
                      specified in the environment variable 'CONSUL_HOST'. If not specified in either
                      location, a DiscoveryError will be raised.
@@ -38,15 +50,7 @@ def discover_endpoints(service_name, consul_host=None, consul_port=None, wait=Fa
       delay --       The number of seconds to wait between rechecking consul for 'service_name' if it
                      was not found the first time, and 'wait' is True.
    ''' 
-   if consul_host is None and 'CONSUL_HOST' in os.environ:
-      consul_host = os.environ['CONSUL_HOST']
-   else:
-      raise DiscoveryError("Environment variable 'CONSUL_HOST' must be provided when 'consul_host' parameter is not provided" % service_name)
-   if consul_port is None and 'CONSUL_PORT' in os.environ:
-      consul_port = os.environ['CONSUL_PORT']
-   else:
-      raise DiscoveryError("Environment variable 'CONSUL_PORT' must be provided when 'consul_port' parameter is not provided" % service_name)
-   
+   consul_host, consul_port = _get_consul(consul_host, consul_port)
    service_ready = False
    while not service_ready:
       service_ready = service_name in get_services(consul_host, consul_port)
@@ -56,7 +60,30 @@ def discover_endpoints(service_name, consul_host=None, consul_port=None, wait=Fa
             time.sleep(delay)
          else:
             raise DiscoveryError("Service '%s' not available yet" % service_name)
+      else:
+         return True
+      
+def discover_endpoints(service_name, consul_host=None, consul_port=None, wait=False, delay=0.1):
+   '''Looks up 'service_name' in Consul and returns an array of tuples describing
+      that service's endpoints in the form (hostname, IP, port).
 
+      Keyword arguments:
+      service_name --the name of the service as advertised in Consul.
+      consul_host -- the IP or host at which consul can be found. If not specified, this should be
+                     specified in the environment variable 'CONSUL_HOST'. If not specified in either
+                     location, a DiscoveryError will be raised.
+      consul_port -- the port at which consul can be found. If not specified, this should be
+                     specified in the environment variable 'CONSUL_PORT'. If not specified in either
+                     location, a DiscoveryError will be raised.
+      wait --        Specifies that if 'service_name' is not found in consul, to block until it is 
+                     (default=False).
+      delay --       The number of seconds to wait between rechecking consul for 'service_name' if it
+                     was not found the first time, and 'wait' is True.
+   ''' 
+   consul_host, consul_port = _get_consul(consul_host, consul_port)
+   
+   service_ready(service_name, consul_host, consul_port, wait, delay)
+   
    url = "http://%s:%d/v1/health/service/%s" % (consul_host, int(consul_port), service_name)
    response = requests.get(url)
    response.raise_for_status()
@@ -74,6 +101,7 @@ def discover_endpoint(service_name, consul_host=None, consul_port=None, wait=Fal
    ''' Returns the first endpoint matching the provided service name.
 
    Keyword arguments:
+      service_name --the name of the service as advertised in Consul.
       consul_host -- the IP or host at which consul can be found. If not specified, this should be
                      specified in the environment variable 'CONSUL_HOST'. If not specified in either
                      location, a DiscoveryError will be raised.
