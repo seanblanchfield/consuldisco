9bb8a0c4f6567934272e4c37db55836dd80077c5
Sean Blanchfield <sblanchfield@gmail.com>
2019-08-27 18:37:03 +0100

    Added dependency on requests library.

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/seanblanchfield/consuldisco",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
